1573c629a0c85f80a1f0a5db8895155ae5bca204
Tam <tam.tahirli@gmail.com>
2021-02-05 20:18:24 -0500

    Initial commit. Added Timmy the soccer ball kid, soccer ball, field, and some quickmaths for projectile motion from the lab as well as the ability to add your own theta with up and down arrow. LMB to kick ball

---
+++ Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target = null;
+    [SerializeField]
+    private float cameraSpeed = 0.5f;
+    [SerializeField]
+    private float upDist = 5;
+    [SerializeField]
+    private float behindDist = 2;
+    [SerializeField]
+    private float rightDist = 2;
+    [SerializeField]
+    private float lookSensitivity = 0.2f;
+
+    // simple script to follow character from behind
+
+    void Update()
+    {
+        Vector3 camPos = (target.transform.forward * -behindDist) + (target.transform.up * upDist) + (target.transform.right * rightDist);
+        transform.position = Vector3.Lerp(transform.position, camPos, cameraSpeed * Time.deltaTime);
+        transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/_Scripts/PhysicsController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject soccerBall;
+    [SerializeField]
+    private Vector3 landingPosition = Vector3.zero;
+    [SerializeField]
+    private float maxHeight = 2;
+    [SerializeField]
+    private float theta = 0;
+    [SerializeField]
+    private bool lowAngle;
+
+    private Animator tAnimator;
+
+    private Vector3 startPosition = Vector3.zero;
+    private Rigidbody soccerRigid;
+
+    void Start()
+    {
+        tAnimator = GetComponent<Animator>();
+        soccerRigid = soccerBall.GetComponent<Rigidbody>();
+        startPosition = soccerBall.transform.position;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            soccerBall.transform.position = startPosition;
+            soccerRigid.velocity = Vector3.zero;
+            tAnimator.Play("KickSoccer");
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            theta = Mathf.Clamp(theta + 1, 0, 80);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            theta = Mathf.Clamp(theta - 1, 0, 80);
+        }
+    }
+
+    public void KickSoccerBall()
+    {
+        // end height
+        float height = soccerBall.transform.position.y;
+        // mag of pos - pos
+        float distance = Vector3.Distance(landingPosition, soccerBall.transform.position);
+        // tan-1(4h / dist)
+        float tTheta = Mathf.Atan((4 * height) / (distance));
+        // vel mag sqrt(2gh) / sin(theta)
+
+        if (theta > 0)
+        {
+            tTheta = theta;
+        }
+
+        float velocityMagnitude = Mathf.Sqrt((2 * Mathf.Abs(Physics.gravity.y) * height)) / Mathf.Sin(tTheta);
+
+        Vector3 finalVel = Vector3.zero;
+
+        finalVel.y = velocityMagnitude * Mathf.Sin(lowAngle ? -tTheta : tTheta);
+        finalVel.x = -(velocityMagnitude * Mathf.Cos(tTheta));
+
+        soccerRigid.velocity = finalVel;
+    }
+}
